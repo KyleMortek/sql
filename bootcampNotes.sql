88986a69ab2afdf276948a08088ef9f4effdee04
lightBulb17 <lightBulb17@github.com>
2019-01-17 19:50:36 -0500

    6_Assessment2_complete

--- bootcampNotes.sql
+++ bootcampNotes.sql
@@ -323,3 +323,109 @@ on a.first_name = b.last_name
 
 --search manager employ self join in google. usually something that pops up in
 -- an interview 
+--assessment test 2 
+--Q1 How can you retrieve all the information from the cd.facilities table? 
+select * from cd.facilities
+
+--Q2 You want to print out a list of all of the facilities and their cost to
+--   members. How would you retrieve a list of only facility names and costs?
+-- select * from cd.facilities
+select name, membercost from cd.facilities
+
+-- Q3 How can you produce a list of facilities that charge a fee to members?
+-- select * from cd.facilities
+select name,membercost from cd.facilities
+where membercost >0
+
+-- Q4 How can you produce a list of facilities that charge a fee to members,
+-- and that fee is less than 1/50th of the monthly maintenance cost? 
+-- Return the facid, facility name, member cost, and monthly maintenance of the 
+-- facilities in question.
+select facid,name, membercost, monthlymaintenance/50 nm
+from  cd.facilities
+where membercost>0 and membercost< monthlymaintenance/50
+
+-- Q5 How can you produce a list of all facilities with the word 'Tennis' in
+-- their name?
+select * from cd.facilities
+where name like '%Tennis%'
+--------------------------------GETTERS------------------------------------
+-- Q6 How can you retrieve the details of facilities with ID 1 and 5? Try to do
+-- it without using the OR operator
+select * from cd.facilities
+where facid in (1,5)
+-- where name in ('Tennis Court 2', 'Massage Room 2')
+-- where name like '%2'
+
+--------------------DATES---------------------------------
+-- Q7 How can you produce a list of members who joined after the start of
+-- September 2012? Return the memid, surname, firstname, and joindate of the 
+-- members in question.
+select memid, surname,firstname,joindate from cd.members
+where joindate > 'Sept 01 2012'
+
+--------------------group by removes duplicates
+-- Q8 How can you produce an ordered list of the first 10 surnames in the 
+-- members table? The list must not contain duplicates.
+-- seems as though group by removes duplicates 
+SELECT surname from cd.members
+group by surname
+order by surname
+limit 10
+
+-- Q9 You'd like to get the signup date of your last member. How can you
+-- retrieve this information?
+select joindate from cd.members
+order by joindate desc
+limit 1
+
+-- Q10 Produce a count of the number of facilities that have a cost to guests 
+-- of 10 or more.
+select count(guestcost) from cd.facilities
+where guestcost >10
+
+-- Q11 skip
+
+-- Q12 Produce a list of the total number of slots booked per facility in the
+-- month of September 2012. Produce an output table consisting of facility id 
+-- and slots, sorted by the number of slots.
+select facid, sum(slots) as total_slots
+from cd.bookings
+where starttime > 'Sept 01 2012'
+group by facid
+order by total_slots
+
+-- Q13 Produce a list of facilities with more than 1000 slots booked. Produce
+-- an output table consisting of facility id and total slots, sorted by 
+-- facility id.
+
+-- select * from cd.facilities
+-- select * from cd.bookings
+select cd.facilities.facid, cd.facilities.name,sum(cd.bookings.slots)
+from cd.bookings
+INNER JOIN cd.facilities
+ON cd.facilities.facid= cd.bookings.facid
+group by cd.facilities.facid
+having sum(slots) >1000
+order by cd.facilities.facid
+
+-- Q14 How can you produce a list of the start times for bookings for tennis
+-- courts, for the date '2012-09-21'? Return a list of start time and facility
+-- name pairings, ordered by the time.
+SELECT cd.facilities.name,cd.bookings.starttime
+from cd.bookings
+inner join cd.facilities
+on cd.facilities.facid = cd.bookings.facid
+where cd.bookings.starttime Between 'Sept 21 2012' and 'Sept 22 2012'
+and cd.facilities.name ilike 'tennis%'
+order by starttime
+
+-- Q15 How can you produce a list of the start times for bookings by members 
+-- named 'David Farrell'?
+select 
+starttime,cd.members.memid,cd.members.firstname ||' '|| cd.members.surname 
+as first_last
+from cd.bookings
+inner join cd.members
+on cd.members.memid = cd.bookings.memid
+where firstname = 'David' and surname ='Farrell' 
