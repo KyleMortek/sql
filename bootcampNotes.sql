17506b2e99daf73e91e1925c2a39cec8f3ff6de1
lightBulb17 <lightBulb17@github.com>
2019-01-19 14:00:39 -0500

    7_create_table
    create table material complete

--- bootcampNotes.sql
+++ bootcampNotes.sql
@@ -429,3 +429,49 @@ from cd.bookings
 inner join cd.members
 on cd.members.memid = cd.bookings.memid
 where firstname = 'David' and surname ='Farrell' 
+
+
+---------------------CREATE TABLE STATEMENT---------------------------------
+CREATE TABLE table_name (
+    column_name datatype PRIMARY KEY,
+    coulmn_name data_type,.....
+)
+
+CREATE TABLE account(
+    user_id serial PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(355) UNIQUE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP
+);
+CREATE TABLE role(
+    role_id serial PRIMARY KEY,
+    role_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE account_role(
+    user_id integer NOT NULL,
+    role_id integer NOT NULL,
+    grant_date TIMESTAMP WITHOUT TIME ZONE,
+    PRIMARY KEY (user_id,role_id),
+
+    CONSTRAINT account_role_role_id_fkey FOREIGN KEY (role_id)
+        REFERENCES role (role_id) MATCH SIMPLE
+        on UPDATE NO ACTION ON DELETE NO ACTION,
+
+    CONSTRAINT account_role_user_id_fkey FOREIGN KEY(user_id)
+        REFERENCES account (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+------------- create table challenege -----------------------------------
+CREATE TABLE leads(
+    user_id serial PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT null,
+    email VARCHAR(355) UNIQUE NOT NULL,
+    minutes integer NOT NULL, 
+    sign_up_date TIMESTAMP NOT NULL
+)
+--------------------------------crete table section complete --------------
